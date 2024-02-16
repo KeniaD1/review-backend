af6b5b99a159a8decca784cf013073b26dae8c70
keniaDE <decoteaukenia@gmail.com>
2024-02-16 14:01:03 -0500

    updated controller functionality

--- review-Backend/controllers/makeupController.js
+++ review-Backend/controllers/makeupController.js
@@ -8,4 +8,20 @@ makeUp.get("/", (req,res) => {
     ({message: "MakeUP Home Page"})
 })
 
+
+makeUp.get("/:makeupID", (req, res) => {
+    const makeupID = req.params.makeupID
+if(Number(makeupID)){
+    res.status(200).json({message : makeupID})
+}
+else{
+
+    res.status(404).json({error: "id must be numeric"})
+}})
+
+
+
+
+
+
 module.exports = makeUp
