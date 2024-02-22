c745da0baa550b5f00b91cc537cb5c88d6126125
keniaDE <decoteaukenia@gmail.com>
2024-02-22 11:49:11 -0500

    updated db config with password

--- review-Backend/db/dbConfig.js
+++ review-Backend/db/dbConfig.js
@@ -3,10 +3,11 @@ require('dotenv').config()
 
 
 const cn = {
-    host: process.env.PG_HOST ,
-    port : process.env.PG_PORT,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
     user: process.env.PG_USER,
-    database: process.env.PG_DATABASE
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD
 }
 
 const db = pgp(cn)
@@ -15,4 +16,4 @@ const db = pgp(cn)
 
 
 
-module.exports= db
+module.exports = db
--- review-Backend/db/seed.sql
+++ review-Backend/db/seed.sql
@@ -1,7 +1,7 @@
 \c makeup_dev;
 
 INSERT INTO makeup (
-product_name, price , client , color ) VALUES
+product_name, price , instock , color ) VALUES
 ('Red Lippie', 25 , true, 'Red'),
 ('Blue Shadow', 10, false, 'Blue'),
 ('Concealer', 30, true, 'Mahoghany');
