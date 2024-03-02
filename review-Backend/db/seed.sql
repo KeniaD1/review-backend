27a52b2acb7c6310a49e9948ddea4e8f17fcc118
keniaDE <decoteaukenia@gmail.com>
2024-03-02 18:13:51 -0500

    update

--- review-Backend/db/seed.sql
+++ review-Backend/db/seed.sql
@@ -6,6 +6,6 @@ product_name, price , instock , color, image_url ) VALUES
 ('Blue Shadow', 10, false, 'Blue', 'https://www.sephora.com/productimages/sku/s2417939-main-zoom.jpg?imwidth=630'),
 ('Concealer', 30, true, 'Mahoghany','https://www.sephora.com/productimages/sku/s2084549-main-zoom.jpg?imwidth=630'),
 ('Mascara', 15, true, 'Black', 'https://www.sephora.com/productimages/sku/s2656130-main-zoom.jpg?imwidth=630'),
-  ('Eyeliner', 12, true, 'Brown', 'https://www.sephora.com/productimages/sku/s1393693-main-zoom.jpg?imwidth=630'),
-  ('Highlighter Palette', 40, true, 'Various', 'https://media.ulta.com/i/ulta/2536087?w=1310&h=1310'),
-  ('Blush', 18, true, 'Pink', 'https://www.sephora.com/productimages/sku/s2352748-main-zoom.jpg?imwidth=630');
+('Eyeliner', 12, true, 'Brown', 'https://www.sephora.com/productimages/sku/s1393693-main-zoom.jpg?imwidth=630'),
+('Highlighter Palette', 40, true, 'Various', 'https://media.ulta.com/i/ulta/2536087?w=1310&h=1310'),
+('Blush', 18, true, 'Pink', 'https://www.sephora.com/productimages/sku/s2352748-main-zoom.jpg?imwidth=630');
