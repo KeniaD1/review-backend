62905998680de46f5a01d599bc53cb8005b0a921
keniaDE <decoteaukenia@gmail.com>
2024-03-02 16:04:03 -0500

    updated seed.sql file with image_url

--- review-Backend/db/seed.sql
+++ review-Backend/db/seed.sql
@@ -1,7 +1,13 @@
 \c makeup_dev;
 
 INSERT INTO makeup (
-product_name, price , instock , color ) VALUES
-('Red Lippie', 25 , true, 'Red'),
-('Blue Shadow', 10, false, 'Blue'),
-('Concealer', 30, true, 'Mahoghany');
+product_name, price , instock , color, image_url ) VALUES
+('Red Lippie', 25 , true, 'Red', 'https://www.sephora.com/productimages/sku/s2655850-av-2-zoom.jpg?imwidth=630'),
+('Blue Shadow', 10, false, 'Blue', 'https://www.sephora.com/productimages/sku/s2417939-main-zoom.jpg?imwidth=630'),
+('Concealer', 30, true, 'Mahoghany','https://www.sephora.com/productimages/sku/s2084549-main-zoom.jpg?imwidth=630'),
+('Mascara', 15, true, 'Black', 'https://www.sephora.com/productimages/sku/s2656130-main-zoom.jpg?imwidth=630'),
+  ('Eyeliner', 12, true, 'Brown', 'https://www.sephora.com/productimages/sku/s1393693-main-zoom.jpg?imwidth=630'),
+  ('Highlighter Palette', 40, true, 'Various', 'https://media.ulta.com/i/ulta/2536087?w=1310&h=1310'),
+  ('Blush', 18, true, 'Pink', 'https://www.sephora.com/productimages/sku/s2352748-main-zoom.jpg?imwidth=630'),
+ 
+;
